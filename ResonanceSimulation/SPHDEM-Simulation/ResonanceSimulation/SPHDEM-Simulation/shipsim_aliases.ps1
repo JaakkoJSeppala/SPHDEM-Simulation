4364e86f3b73fd14706df6643cf804fb281c0b7e
Jaakko Seppälä <jaakko.j.seppala@gmail.com>
2026-01-26 10:47:16 +0200

    Delete ResonanceSimulation directory

--- ResonanceSimulation/SPHDEM-Simulation/ResonanceSimulation/SPHDEM-Simulation/shipsim_aliases.ps1
+++
@@ -1,3 +0,0 @@
-# Add these lines to your PowerShell profile to create global aliases
-Set-Alias runshipsim "C:\Users\jaakk\Desktop\Gradu\runshipsim.ps1"
-Set-Alias compileshipsim "C:\Users\jaakk\Desktop\Gradu\compileshipsim.ps1"
